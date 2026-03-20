f3c833e4689db123e72b5ac4c755c8078ab42560
Minseok Choi <renardtoucan@gmail.com>
2026-03-19 23:07:29 -0400

    added GameManager singleton

---
+++ StellarCommand/Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace StellarCommand.Core
+{
+    public class GameManager : MonoSingleton<GameManager>
+    {
+        protected override void OnInitialize()
+        {
+        }
+    }
+}
